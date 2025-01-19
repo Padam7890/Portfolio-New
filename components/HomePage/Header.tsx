2b576a393cad3f2fede7b2098b0deab739594c06
Padam7890 <padamthapamoho@gmail.com>
2025-01-19 21:12:41 +0545

    feat: responsive nav added

--- components/HomePage/Header.tsx
+++ components/HomePage/Header.tsx
@@ -3,36 +3,78 @@ import { menuLists } from "@/constent/Menu";
 import { menuItem } from "@/types/homepage";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const Header = () => {
   const pathname = usePathname();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
-    <>
-      <section className=" w-full h-auto card-bg mt-9  sticky  top-5  backdrop-blur-md bg-[#111111]/30 ">
-        <div className="bg-opacity-20 flex justify-between items-center">
-          <div className="logo heading-1">Padam</div>
-          <div>
-            <ul className="flex text-base_white font-medium uppercase gap-9">
-              {menuLists.map((menu: menuItem, index: number) => (
-                <li key={index}>
-                  <Link
-                    href={menu.url}
-                    passHref
-                    className={` navigation-menu  bg-black bg-opacity-5 px-6 py-2 hover:text-white ${
-                      pathname === menu.url
-                        ? " border white-50  border-[#cccccc3e] rounded-full bg-opacity-40"
-                        : " white-70"
-                    }`}
-                  >
-                    {menu.name}
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+    <section
+      className={`w-full h-auto card-bg mt-9 sticky top-5 p-3 md:p-8 bg-base_black md:bg-[#111111]/30 md:backdrop-blur-md  backdrop-blur-none ${
+        isMenuOpen ? "backdrop-blur-none" : " backdrop-blur-2xl bg-[#111111]/90"
+      }`}
+    >
+      <div className="bg-opacity-20 flex justify-between items-center">
+        <div className="logo heading-1">Padam</div>
+        <div className="block md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="text-white p-2 rounded-md focus:outline-none"
+          >
+            <span className="block w-6 h-0.5 bg-white mb-1"></span>
+            <span className="block w-6 h-0.5 bg-white mb-1"></span>
+            <span className="block w-6 h-0.5 bg-white"></span>
+          </button>
         </div>
-      </section>
-    </>
+
+        <div className="hidden md:block">
+          <ul className="flex text-base_white font-medium uppercase gap-9">
+            {menuLists.map((menu: menuItem, index: number) => (
+              <li key={index}>
+                <Link
+                  href={menu.url}
+                  passHref
+                  className={`navigation-menu  bg-opacity-5 px-6 py-2 hover:text-white ${
+                    pathname === menu.url
+                      ? "border white-50 border-[#cccccc3e] rounded-full "
+                      : "white-70"
+                  }`}
+                >
+                  {menu.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+
+      <div
+        className={`md:hidden  bg-opacity-70 text-white transition-all duration-300 ease-in-out ${
+          isMenuOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"
+        } overflow-hidden`}
+      >
+        <ul className="flex flex-col text-left font-medium uppercase gap-9 py-5">
+          {menuLists.map((menu: menuItem, index: number) => (
+            <li key={index}>
+              <Link
+                href={menu.url}
+                passHref
+                className={`navigation-menu  bg-opacity-5 px-6 py-2 hover:text-white ${
+                  pathname === menu.url
+                    ? "border white-50 border-[#cccccc3e] rounded-full "
+                    : "white-70"
+                }`}
+              >
+                {menu.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </section>
   );
 };
 
